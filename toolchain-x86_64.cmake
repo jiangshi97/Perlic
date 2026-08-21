1d914975ef218238d1df9adfa4832b8766a39885
revHe <wdsjhecheng@163.com>
2026-08-21 12:24:15 +0800

    add cpp support

--- toolchain-x86_64.cmake
+++ toolchain-x86_64.cmake
@@ -6,7 +6,8 @@ set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 # 指定交叉编译器，这里以 x86_64 裸机工具链为例
 # 你可以根据实际安装的编译器修改，如 i686-elf-gcc
-set(CMAKE_C_COMPILER gcc)
+set(CMAKE_C_COMPILER x86_64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER x86_64-linux-gnu-g++)
 set(CMAKE_ASM_COMPILER nasm) # 如果使用 NASM
 
 # 告诉 CMake 不要尝试为宿主机系统进行链接测试
